458a237e255f4a6a5ef3951c5a950df73ad39880
Axel Hahn <ax2002@gmx.net>
2019-12-30 17:40:42 +0100

    2019-12-30: v2.04.00   * ADDED: new CLI tool to configure program and setup groups and servers   * ADDED: cli class   * UPDATE: php docs in class configserver   * UPDATE: confighandler class   * UPDATE: bookmarklet button on the help page   * UPDATE: Chart.js to 2.9.3

--- views/serverinfos.php
+++ views/serverinfos.php
@@ -6,7 +6,23 @@
  */
 
 
+$aGroupLinks=array();
+if(isset($aEnv['links']['servers']) && count($aEnv['links']['servers'])){
+    
+    foreach($aEnv['links']['servers'] as $sGroup=>$aMembers){
+        $sNewUrl=getNewQs(array('group'=>$sGroup));
+        $aGroupLinks[]=array(
+            'active'=>$sGroup===$aEnv['active']['group'],
+            'url'=>$sNewUrl,
+            'label'=>$aCfg['icons']['group'].$sGroup
+        );
+    }
+}
+// echo '<pre>'.print_r($aEnv, 1).'</pre>';
+// echo '<pre>'.print_r($aTC, 1).'</pre>';
+
 $content=
+    $oDatarenderer->renderTabs($aGroupLinks).
     // $oDatarenderer->themeTable($aLangTxt["lblTable_status_workers"], $oDatarenderer->renderWorkersTable($aSrvStatus), $aLangTxt["lblTableHint_status_workers"]).
     // $oDatarenderer->renderTable('workers_table');
     $oDatarenderer->renderGroupAndServers($aSrvStatus).
