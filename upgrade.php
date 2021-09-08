1b787ffd8674f12db2f8f13e0dbea32e29fc11da
Axel Hahn <ax2002@gmx.net>
2021-09-08 23:46:43 +0200

    2021-09-08: v2.04.08   * FIX: upgrade script for config of v1 to v2 (https://github.com/axelhahn/pimped-apache-status/issues/31)   * UPDATE: font-awesome to 5.15.4

--- upgrade.php
+++ upgrade.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <?php
 /*
  * PIMPED APACHE-STATUS
@@ -6,13 +7,17 @@
  * 
  */
 
+// ----------------------------------------------------------------------
+// INIT
+// ----------------------------------------------------------------------
 require_once './classes/confighandler.class.php';
-$oCfg = new confighandler("internal-env");
 global $aServergroups, $aCfg;
 require_once './classes/cdnorlocal.class.php';
 $oCdn=new axelhahn\cdnorlocal();
 
-
+// ----------------------------------------------------------------------
+// FUNCTIONS
+// ----------------------------------------------------------------------
 function renameOld($filename){
     $bakfile=dirname($filename).'/__2_delete_'.basename($filename);
     if (file_exists($bakfile)){
@@ -29,62 +34,71 @@ function renameOld($filename){
     }
     return true;
 }
+
+
+// ----------------------------------------------------------------------
+// MAIN
+// ----------------------------------------------------------------------
+$oCfg = new axelhahn\confighandler("internal-env");
+$aEnv=$oCfg->getFullConfig();
+$oCdn->setLibs($aEnv['vendor']);
+
 ?>
 <html>
     <head>
-        <link href="<?php echo $oCdn->getFullUrl('font-awesome/5.11.2/css/all.min.css'); ?>" rel="stylesheet">
+        <link href="<?php echo $oCdn->getFullUrl($oCdn->getLibRelpath('font-awesome').'/css/all.min.css'); ?>" rel="stylesheet">
+        <style>
+            body{color:#333; background:#eee; background: linear-gradient(-10deg, #abc,#fff,#ccc) fixed; font-family: "arial";}
+            body>div{margin: 1em 20% 3em; background: #f8f8f8; border: 2px solid #aaa; border-radius: 1em; box-shadow: 0 0 3em #888; padding: 1em 1em 3em;}
+            a{color:#88c; padding: 0.3em; background: #e0e0ff ; border: 1px solid #ccc; text-decoration: none;}
+            h1{color:#89c;}
+            h2{color:#67a; margin: 2em 0 0;}
+            .error{color:#a00;}
+            .file{color:#970;}
+        </style>
     </head>
-</html>
-<style>
-    body{color:#333; background:#eee; background: linear-gradient(-10deg, #abc,#fff,#ccc) fixed; font-family: "arial";}
-    body>div{margin: 1em 20% 3em; background: #f8f8f8; border: 2px solid #aaa; border-radius: 1em; box-shadow: 0 0 3em #888; padding: 1em 1em 3em;}
-    a{color:#88c; padding: 0.3em; background: #e0e0ff ; border: 1px solid #ccc; text-decoration: none;}
-    h1{color:#89c;}
-    h2{color:#67a; margin: 2em 0 0;}
-    .error{color:#a00;}
-    .file{color:#970;}
-</style>
-<body>
-    <div>
-        <h1><i class="fas fa-magic" aria-hidden="true"></i> Pimped Apache Status :: Converter for config files</h1>
-         This converter upgrades the config files from version 1.x to 2.x<br>
-         It creates JSON files in config directory and renames the old configs.
+    <body>
+        <div>
+            <h1><i class="fas fa-magic" aria-hidden="true"></i> Pimped Apache Status :: Converter for config files</h1>
+             This converter upgrades the config files from version 1.x to 2.x<br>
+             It creates JSON files in config directory and renames the old configs.
 
-        <h2>Loading old config</h2>
-        <?php
+            <h2>Loading old config</h2>
+            <?php
 
-        // ---------- load 
-        if (!@include("config/config_user.php")) {
-            echo "No user config <span class=\"file\">config/config_user.php</span> was found.<br>"
-            . "Maybe the the converter was executed already.<br>";
-        }else {
-            echo "OK, <span class=\"file\">config/config_user.php</span> was read.";
-            if(!isset($aCfg['auth']) || !count($aCfg['auth'])){
-                    $aCfg['auth']=false;
+            // ---------- load 
+            if (!@include("config/config_user.php")) {
+                echo "No user config <span class=\"file\">config/config_user.php</span> was found.<br>"
+                . "Maybe the the converter was executed already.<br>";
+            }else {
+                echo "OK, <span class=\"file\">config/config_user.php</span> was read.";
+                if(!isset($aCfg['auth']) || !count($aCfg['auth'])){
+                        $aCfg['auth']=false;
+                }
+
+                // ---------- write
+                echo "<h2>Writing new config</h2>";
+                $oCfg->configSet("config_servers");
+                $oCfg->set($aServergroups);
+                echo "OK, <span class=\"file\">config/config_servers.json</span> was written.<br>";
+                $oCfg->configSet("config_user");
+                $oCfg->set($aCfg);
+                echo "OK, <span class=\"file\">config/config_user.json</span> was written.<br>";
             }
 
-            // ---------- write
-            echo "<h2>Writing new config</h2>";
-            $oCfg->configSet("config_servers");
-            $oCfg->set($aServergroups);
-            echo "OK, <span class=\"file\">config/config_servers.json</span> was written.<br>";
-            $oCfg->configSet("config_user");
-            $oCfg->set($aCfg);
-            echo "OK, <span class=\"file\">config/config_user.json</span> was written.<br>";
-        }
+                // ---------- remove old
+                echo "<h2>Removing old config</h2>";
+                // renameOld("config/config_default.php");
+                renameOld("config/config_user.php");
+                // renameOld("config/config_user_default.php");
 
-            // ---------- remove old
-            echo "<h2>Removing old config</h2>";
-            // renameOld("config/config_default.php");
-            renameOld("config/config_user.php");
-            // renameOld("config/config_user_default.php");
+            // ---------- done
+            ?>
 
-        // ---------- done
-        ?>
-
-        <h2>Done</h2>
-        go to Pimped Apache Status
-            <a href="./"><i class="fas fa-tachometer-alt" aria-hidden="true"></i> Monitoring</a>
-            <a href="./admin/?&action=settings"><i class="fas fa-cog"></i> Admin .. Settings</a><br>
-    </div>
-</body>
+            <h2>Done</h2>
+            go to Pimped Apache Status
+                <a href="./"><i class="fas fa-tachometer-alt" aria-hidden="true"></i> Monitoring</a>
+                <a href="./admin/?&action=settings"><i class="fas fa-cog"></i> Admin .. Settings</a><br>
+        </div>
+    </body>
+</html>
