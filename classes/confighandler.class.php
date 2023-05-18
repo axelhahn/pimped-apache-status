5cca899145de54e1e3f22b7842b0a1f13adbdd55
Axel Hahn <ax2002@gmx.net>
2023-05-18 22:50:24 +0200

    update phpdoc

--- classes/confighandler.class.php
+++ classes/confighandler.class.php
@@ -193,7 +193,7 @@ class confighandler {
      * @see setCfgId()
      * 
      * @param string  $sId  optional: id of config (without extension)
-     * @return type
+     * @return array
      */
     public function getFullConfig($sId=false) {
         if ($sId){
